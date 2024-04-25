3e339b94ad78175ff700e077e7e35a934641203c
Ramiro Martin <ramartin@despegar.com>
2024-04-25 20:33:12 -0300

    Fixture

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ matchProcessor = MatchProcessor.MatchProcessor("data/futbol_martes.csv")
 
 @app.route("/")
 def home():
-    ##return render_template("promiedos.jinja2", players=matchProcessor.get_position_table())
     return render_template("v2/table.jinja2", players=matchProcessor.get_position_table())
 
 
@@ -21,11 +20,10 @@ def match(match_id):
 @app.route("/matches/<int:match_day>")
 def matches(match_day):
     match_id = match_day - 1
-    return render_template("canchita.jinja2",
+    return render_template("v2/fixture.jinja2",
                            match=matchProcessor.matches[match_id],
                            match_id = match_id,
-                           total_matches=list(range(0, len(matchProcessor.matches))))
-
+                            total_matches=list(range(0, len(matchProcessor.matches))))
 
 @app.route("/match")
 def matches2():
